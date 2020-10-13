cb990b85da506db929569153f6b59e851b21a7d4
Muhammad Alfin <71116571+finpangestu@users.noreply.github.com>
2020-10-13 16:01:13 +0700

    Create biodata.php

---
+++ biodata.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang=”en”>
+<head>
+<meta charset=”UTF-8">
+<meta name=”viewport” content=”width=device-width, initial-scale=1.0">
+<title>Biodata</title>
+</head>
+<body>
+<strong><h2>BIODATA</h2></strong>
+<strong>Data diri</strong>
+<table>
+<tr>
+<td>Nama</td>
+<td> : <?php echo “Muhammad Alfin PAngestu”; ?> </td>
+</tr>
+<tr>
+<td>Tempat, Tanggal lahir </td>
+<td> : <?php echo “Lamongan, 06 November 1999”; ?> </td>
+</tr>
+<tr>
+<td>Alamat asal</td>
+<td> : <?php echo “Surabaya, Jawa Timur”; ?> </td>
+</tr>
+<tr>
+<td>Alamat sekarang</td>
+<td> : <?php echo “Yogyakarta”; ?> </td>
+</tr>
+<tr>
+<td>Email</td>
+<td> : <?php echo “195410015@students.akakom.ac.id”; ?> </td>
+</tr>
+<br></br>
+</table>
+<br></br>
+<strong>Pendidikan</strong>
+<br></br>
+<table>
+<tr>
+<td>Asal SMP</td>
+<td> : <?php echo “SMPN 5 Surabaya”; ?> </td>
+</tr>
+<tr>
+<td>Asal SMK</td>
+<td> : <?php echo “SMKS Pawiyatan Surabaya”; ?> </td>
+</tr>
+<tr>
+<td>Sekarang</td>
+<td> : <?php echo “STMIK Akakom Yogyakarta”; ?> </td>
+</tr>
+</table>
+</body>
+</html>
